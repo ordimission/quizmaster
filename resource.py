82ace8630b90c18f01382a7086fee5f8e5012364
fabrice <fab0669@gmail.com>
2014-11-15 15:44:17 +0100

    multiple databases support

--- resource.py
+++ resource.py
@@ -4,27 +4,26 @@ import util
 from flask.ext.restful import Resource, marshal_with, reqparse 
 from format import *
 
-db = eval('mongo_db')
+db = eval('tiny_db')
 
 # Questionnaire resource management
-class Questionnaire(Resource):
-    @marshal_with(questionnaire_format)
-    #def __init__(self):
 
-    def get(self,id):
-	questionnaire = db.get_questionnaire(id)
-        self.req_parser = reqparse.RequestParser()
-	self.req_parser.add_argument('shuffle', default=0, type=int, required=False)
-        self.req_parser.add_argument('size', default=len(questionnaire['questions']), type=int, required=False)
-        self.args = self.req_parser.parse_args()
-        if not self.args['shuffle'] == 0:
-	    ordered_questions = questionnaire['questions'];
-	    quizz_questions = util.shuffle(ordered_questions, self.args['size'] )
-	    questionnaire['questions'] = quizz_questions
-        return questionnaire
+class Questionnaire(Resource):
+	@marshal_with(questionnaire_format)
+	def get(self,id):
+		questionnaire = db.get_questionnaire(id)
+		self.req_parser = reqparse.RequestParser()
+		self.req_parser.add_argument('shuffle', default=0, type=int, required=False)
+		self.req_parser.add_argument('size', default=len(questionnaire['questions']), type=int, required=False)
+		self.args = self.req_parser.parse_args()
+		if not self.args['shuffle'] == 0:
+			ordered_questions = questionnaire['questions'];
+			quizz_questions = util.shuffle(ordered_questions, self.args['size'] )
+			questionnaire['questions'] = quizz_questions
+		return questionnaire
 
 # QuestionnaireList global resource management
 class QuestionnaireList(Resource):
-    @marshal_with(questionnaire_flat_format)
-    def get(self):
-        return db.get_questionnaires()
+	@marshal_with(questionnaire_flat_format)
+	def get(self):
+		return db.get_questionnaires()
--- tiny_db.py
+++ tiny_db.py
@@ -1,15 +1,27 @@
 from tinydb import TinyDB, where
+import json
 
 db = TinyDB('db/tinydb.json')
 
+
 def create_all(script_name):
     print "Importing "+script_name
+    json_questionnaires=open(script_name).read()
+    questionnaires = json.loads(json_questionnaires)
+    for questionnaire in questionnaires:
+        db.insert(questionnaire)
+
+
 
 def get_questionnaire(id):
-    return db.search(where('_id')==id)
+    questionnaires_matching_id = db.search(where('_id')==id)
+    return questionnaires_matching_id[0]
 
 
 def get_questionnaires():
     return list(db.all())
 
+
+if __name__ == '__main__':
+    create_all('db/questionnaires.json')
 
